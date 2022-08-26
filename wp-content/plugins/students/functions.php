c51e8896e4c6c99cd1e58c482d687935c8c258b7
abozhiinov <abozhiinov@gmail.com>
2022-08-26 16:36:33 +0300

    add/update/delete student

--- wp-content/plugins/students/functions.php
+++ wp-content/plugins/students/functions.php
@@ -40,4 +40,170 @@ add_action( 'rest_api_init', function () {
     ) );
 } );
 
+/**
+ * Register adding student route
+ */
+add_action( 'rest_api_init', function () {
+  register_rest_route( '/add-student', '/v2', array(
+        'methods'  => 'POST',
+        'callback' => 'add_student_callback',
+    ) );
+} );
+
+/**
+ * Add student through REST API
+ */
+function add_student_callback( $request_data ) {
+    $data = array();
+
+    $parameters  = $request_data->get_params();
+    $post_title  = sanitize_text_field( $parameters['post_title'] );
+    $the_content = sanitize_text_field( $parameters['the_content'] );
+    $the_excerpt = sanitize_text_field( $parameters['the_excerpt'] );
+
+    $lives_in  = sanitize_text_field( $parameters['lives_in'] );
+    $address   = sanitize_text_field( $parameters['address'] );
+    $birthdate = sanitize_text_field( $parameters['birthdate'] );
+    $class     = sanitize_text_field( $parameters['class'] );
+    $status    = sanitize_text_field( $parameters['status'] );
+
+
+    if( !empty( $post_title ) ){
+
+        $my_post = array(
+        'post_title'   => $post_title,
+        'post_content' => $the_content,
+        'post_author'  => '',
+        'post_excerpt' => $the_excerpt,
+        'post_status'  => 'publish',
+        'post_type'    => 'student',
+        );
+        $new_post_id = wp_insert_post( $my_post );
+
+        update_post_meta( $new_post_id, 'lives_in', $lives_in) ; 
+        update_post_meta( $new_post_id, 'address', $address );
+        update_post_meta( $new_post_id, 'birthdate', $birthdate );
+        update_post_meta( $new_post_id, 'class', $class );
+        update_post_meta( $new_post_id, 'status', $status );
+
+        if ($new_post_id) {
+            $data[ 'status' ] = 'Post added Successfully.';  
+        }
+        else {
+            $data[ 'status' ] = 'post failed..';
+        }
+
+    } else {
+        $data[ 'status' ] = 'Please provide correct post details.';
+    }
+
+    return $data;
+}
+
+/**
+ * Register editing student route
+ */
+add_action( 'rest_api_init', function () {
+    register_rest_route( '/v1', '/edit-student/(?P<id>\d+)', array(
+          'methods'  => 'POST',
+          'callback' => 'edit_student_callback',
+      ) );
+} );
+
+/**
+ * Edit student through REST API
+ */
+function edit_student_callback( $request_data ) {
+    $data = array();
+    $id = basename( cur_page_url(), '?' );
+
+    $parameters  = $request_data->get_params();
+    //var_dump($parameters);
+    if( !empty( $parameters['post_title'] ) )  $post_title  = sanitize_text_field( $parameters['post_title'] );
+    if( !empty( $parameters['the_content'] ) ) $the_content = sanitize_text_field( $parameters['the_content'] );
+    if( !empty( $parameters['the_excerpt'] ) ) $the_excerpt = sanitize_text_field( $parameters['the_excerpt'] );
+
+    if( $parameters['country'] !== null )  { $country  = sanitize_text_field( $parameters['country'] ); }
+    if( !empty( $parameters['address'] ) )  { $address   = sanitize_text_field( $parameters['address'] ); }
+    if( !empty( $parameters['birthdate'] ) ){ $birthdate = sanitize_text_field( $parameters['birthdate'] ); }
+    if( !empty( $parameters['class'] ) )    { $class     = sanitize_text_field( $parameters['class'] ); }
+    if( !empty( $parameters['status'] ) )   { $status    = sanitize_text_field( $parameters['status'] ); }
+
+    if( !empty( $id ) ){
+
+        $my_post = array();
+        $my_post += [ 'ID' => $id, ];
+        if( !empty( $post_title ) )  $my_post += [ 'post_title'   => $post_title, ];
+        if( !empty( $the_content ) ) $my_post += [ 'post_content' => $the_content, ];
+        if( !empty( $the_excerpt ) ) $my_post += [ 'post_excerpt' => $the_excerpt, ];
+
+        $post_id = wp_update_post( $my_post );
+
+        if( isset( $country ) )    update_post_meta( $post_id, 'lives_in', $country) ; 
+        if( !empty( $address ) )   update_post_meta( $post_id, 'address', $address );
+        if( !empty( $birthdate ) ) update_post_meta( $post_id, 'birthdate', $birthdate );
+        if( !empty( $class ) )     update_post_meta( $post_id, 'class', $class );
+        if( !empty( $status ) )    update_post_meta( $post_id, 'status', $status );
+        var_dump(get_post_meta($post_id));
+        if ($post_id) {
+            $data[ 'status' ] = 'Post updated Successfully.';  
+        }
+        else {
+            $data[ 'status' ] = 'post failed..';
+        }
+
+    } else {
+        $data[ 'status' ] = 'Please provide correct post details.';
+    }
+
+    return $data;
+}
+
+/**
+ * Register deleting student route
+ */
+add_action( 'rest_api_init', function () {
+    register_rest_route( '/v1', '/delete-student/(?P<id>\d+)', array(
+          'methods'  => 'POST',
+          'callback' => 'delete_student_callback',
+      ) );
+} );
+
+/**
+ * Delete student through REST API
+ */
+function delete_student_callback( ) {
+    $data = array();
+    $id   = basename( cur_page_url() );
+ 
+    if( !empty( $id ) ){
+        wp_delete_post( $id );
+        $data[ 'status' ] = 'Post deleted Successfully.';  
+    } else {
+        $data[ 'status' ] = 'Please provide correct post details.';
+    }
+
+    return $data;
+}
+
+/**
+ * Get current page URL
+ */
+function cur_page_url() {
+    $pageURL = 'http';
+
+    if ( $_SERVER[ "HTTPS" ] == "on" ) {
+        $pageURL .= "s";
+    }
+    $pageURL .= "://";
+    if ( $_SERVER[ "SERVER_PORT" ] != "80" ) {
+        $pageURL .= $_SERVER[ "SERVER_NAME" ] . ":" . $_SERVER[ "SERVER_PORT" ] . $_SERVER[ "REQUEST_URI" ];
+    } else {
+        $pageURL .= $_SERVER[ "SERVER_NAME" ] . $_SERVER[ "REQUEST_URI" ];
+    }
+
+    return $pageURL;
+}
+
+
 ?>
