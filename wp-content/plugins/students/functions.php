312ce833182156eadfddeb1ca2e685ebf64774ed
abozhiinov <abozhiinov@gmail.com>
2022-08-29 14:45:05 +0300

    coding standards fixes

--- wp-content/plugins/students/functions.php
+++ wp-content/plugins/students/functions.php
@@ -34,7 +34,7 @@ function rest_api_student( $data ) {
 }
 
 add_action( 'rest_api_init', function () {
-    register_rest_route( '/v1', '/student(?:/(?P<id>\d+))?', array(
+    register_rest_route('/v1', '/student(?:/(?P<id>\d+))?', array(
         'methods'  => 'GET',
         'callback' => 'rest_api_student'
     ) );
