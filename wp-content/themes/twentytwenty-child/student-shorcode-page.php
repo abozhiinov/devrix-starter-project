ca9699364515ae91dc31822a338e5ee547ddc254
abozhiinov <abozhiinov@gmail.com>
2022-09-07 16:45:40 +0300

    more coding standards

--- wp-content/themes/twentytwenty-child/student-shorcode-page.php
+++ wp-content/themes/twentytwenty-child/student-shorcode-page.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/**
  * Template Name: Student Shortcode Page
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
@@ -7,21 +7,22 @@
  * @package WordPress
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
-*/
+ */
+
 get_header(); ?>
 
 <header class="archive-header has-text-align-center header-footer-group">
-    <div class="archive-header-inner section-inner medium">
-        <a class="archive-title" href="<?php home_url('students-shortcode'); ?>" >Students Shortcode</a>
-    </div>
+	<div class="archive-header-inner section-inner medium">
+		<a class="archive-title" href="<?php esc_url( home_url( 'students-shortcode' ) ); ?>" >Students Shortcode</a>
+	</div>
 </header>
 
 <div id="site-content">
-    <div id="content" role="main" class="archive-header-inner section-inner medium">
-    <?php
-        the_content();
-    ?>
-    </div>
+	<div id="content" role="main" class="archive-header-inner section-inner medium">
+	<?php
+		the_content();
+	?>
+	</div>
 </div>
 
 <?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
